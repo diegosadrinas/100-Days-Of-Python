baeafde07e00efc858b504349524bd87dbbde73b
Diego Sadrinas <dsadrinas@gmail.com>
2021-06-21 22:09:25 -0300

    added a new functionality to the contact form (send email)

--- Blog-Project/main.py
+++ Blog-Project/main.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request
 import requests
+import smtplib
+import decouple
 
+MY_EMAIL = decouple.config("MY_EMAIL")
+EMAIL_PASS = decouple.config("EMAIL_PASS")
+TO_EMAIL = decouple.config("TO_EMAIL")
 app = Flask(__name__)
 
 posts_url = "https://api.npoint.io/e42b353ee387383898c7"
@@ -28,7 +33,19 @@ def goto_about():
 
 @app.route("/contact", methods=['GET', 'POST'])
 def goto_contact():
-    return render_template("contact.html")
+    if request.method == 'GET':
+        return render_template("contact.html")
+    elif request.method == 'POST':
+        connection = smtplib.SMTP("smtp.gmail.com")
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=EMAIL_PASS)
+        connection.sendmail(from_addr=MY_EMAIL, to_addrs=TO_EMAIL,
+                            msg=f"Subject: Contact Form MSG\n\nYou received the following message from your blog:"
+                                f"Name: {request.form['name']}\nEmail: {request.form['email']}\nPhone: "
+                                f"{request.form['phone']}\nMessage: {request.form['message']}")
+        connection.close()
+        print(request.form['name'], request.form['email'], request.form['phone'], request.form['message'])
+        return render_template("contact.html")
 
 
 @app.route("/post/<int:post_id>")
